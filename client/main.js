8ea3354edddd6892f2878a5bb1ff76a2b5d8b0c8
Denimwearer <CorderoDixon12@yahoo.com>
2023-12-08 17:46:14 -0500

    almost finshed

--- client/main.js
+++ client/main.js
@@ -1,11 +1,82 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
+const body = document.querySelector("body");
+const fortuneBtn = document.createElement("button");
+const form = document.querySelector("form");
 
 const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
+  axios.get("http://localhost:4000/api/compliment").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
+
+const getFortune = () => {
+  axios.get("http://localhost:4000/api/fortune").then((res) => {
+    const data = res.data.text;
+    console.log(data);
+    alert(data);
+  });
+};
+
+const createFortune = (body) => {
+  axios
+    .post("http://localhost:4000/api/fortune", body)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
 
-complimentBtn.addEventListener('click', getCompliment)
+const deleteFortune = (id) => {
+  axios
+    .delete(`http://localhost:4000/api/fortune/${id}`)
+    .then((res) => {
+      const data = res.data;
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const updateFortune = (id, text) => {
+  axios
+    .put(`http://localhost:4000/api/fortune/${id}`, { text })
+    .then((res) => {
+      const data = res.data;
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  let text = document.querySelector("#fortuneText");
+
+  let bodyObj = {
+    text: text.value,
+  };
+
+  createFortune(bodyObj);
+  console.log(bodyObj);
+};
+
+const createFortuneCard = (fortune) => {
+  const fortuneCard = document.createElement("div");
+  fortuneCard.classList.add("fortune-card");
+
+  fortuneCard.innerHTML = `<p> ${fortune.text}</p>`;
+
+  body.appendChild(fortuneCard);
+};
+
+complimentBtn.addEventListener("click", getCompliment);
+fortuneBtn.addEventListener("click", getFortune);
+fortuneBtn.setAttribute("id", "fortuneButton");
+fortuneBtn.textContent = "Get a fortune";
+body.appendChild(fortuneBtn);
+form.addEventListener("submit", submitHandler);
--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,68 @@
-module.exports = {
+const fortunes = require("./db.json");
+let globalId = 7;
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
+module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
+
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
+
+    res.status(200).send(randomCompliment);
+  },
+
+  getFortune: (req, res) => {
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+    console.log(randomFortune);
+  },
+
+  createFortune: (req, res) => {
+    const { text } = req.body;
+
+    let newFortune = {
+      id: globalId,
+      text,
+    };
+    fortunes.push(newFortune);
+    globalId++;
+    res.status(200).send(fortunes);
+  },
+
+  deleteFortune: (req, res) => {
+    let index = fortunes.findIndex((elem) => elem.id === +req.params.id);
+    if (index === -1) {
+      res.status(400).send("Can't delete");
+      return;
     }
 
-}
+    fortunes.splice(index, 1);
+    res.status(200).send(fortunes);
+    return;
+  },
+
+  updateFortune: (req, res) => {
+    const { text } = req.body;
+    let index = fortunes.findIndex((elem) => elem.id === +req.params.id);
+
+    let editFortune = {
+      text,
+    };
+
+    fortunes.push(editFortune);
+    console.log(editFortune);
+    res.status(200).send(fortunes);
+
+    if (index === -1) {
+      res.status(400).send("Can not find fortune");
+      return;
+    }
+  },
+};
